3ad9759b95800f3f2d0d9eb1e5923d2793786fca
Nestor Eduardo <Shadow16@users.noreply.github.com>
2014-04-01 18:07:55 -0600

    Create sesion.php
    Formulario Principal en php

---
+++ sesion.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Sesi&oacuten.php</title>
+	</head>
+	<body>
+		<form method="POST" action="inicio.php">
+			<label for="usuario">Usuario</label><br \>
+			<input type="text" name="usuario"><br \>
+			<label for="password">Contraseña</label><br \>
+			<input type="password" name="password"> <br \><br \>
+			<input type="submit" name="iniciar" value="INICIAR">
+		</form>
+	</body> 
+</html>
